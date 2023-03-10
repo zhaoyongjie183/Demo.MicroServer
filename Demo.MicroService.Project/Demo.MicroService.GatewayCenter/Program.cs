58d0e730fa986662f83218a321b916201379311f
zyj <893722059@qq.com>
2023-03-10 13:41:47 +0800

    feat：ocelot服务监听

--- Demo.MicroService.Project/Demo.MicroService.GatewayCenter/Program.cs
+++ Demo.MicroService.Project/Demo.MicroService.GatewayCenter/Program.cs
@@ -1,7 +1,15 @@
+using Nacos.AspNetCore.V2;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Logging.AddLog4Net();
+#region Nacos配置
+// 注册服务到Nacos
+builder.Services.AddNacosAspNet(builder.Configuration, section: "NacosConfig"); //默认节点Nacos
+//builder.Host.UseNacosConfig(section: "NacosConfig");
+builder.Configuration.AddNacosV2Configuration(builder.Configuration.GetSection("NacosConfig"));
+#endregion
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
